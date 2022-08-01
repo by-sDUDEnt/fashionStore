c7b225b5ab3b3f1f620ba124e9e738f9dd9425fc
sDUDEnt <ia04kuchynskyiboryslav@gmail.com>
2022-08-01 16:51:36 +0300

    signIn/Up v0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Signup from "./components/signUp/Signup";
+import Signin from "./components/SignIn/Signin";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+
+    const login = localStorage.getItem("isLoggedIn");
+
+    return (
+      <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Signup />}></Route>
+                <Route path="/sign-in" element={<Signin />}></Route>
+                <Route path="/home" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
